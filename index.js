1fbffed91e86ab99add91d50899eaa65b0717790
Noah Chirchir <chirchirnoah37@gmail.com>
2024-03-23 14:19:46 +0300

    Modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+// index.js
+
+function writeCards(names, giftType) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${giftType} gift!`);
+  }
+ 
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
